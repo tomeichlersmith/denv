9d48787c66d637e6ead06f8b96f9194062853c63
tomeichlersmith <eichl008@umn.edu>
2024-01-19 10:34:21 -0600

    add a more direct test of internet connectivity
    just see if we can connect to a specific port of a google public DNS
    server, use a python image in the network test and have it share the
    same workspace for the file to save time downloading/unpacking the imag

---
+++ test/internet-access.py
@@ -0,0 +1,24 @@
+"""python3 script to quick check if network is accessible
+
+https://stackoverflow.com/a/33117579
+"""
+
+import socket
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(0 if internet() else 1)
